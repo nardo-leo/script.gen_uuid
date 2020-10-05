abf637f7b41a3abd84d222dd23b0040cc5187dea
nardo-leo <jora.karuzo@gmail.com>
2020-10-05 17:47:01 +0300

    Recursive walk through dirs to rename files then kills itself at the end

--- main.py
+++ main.py
@@ -8,15 +8,26 @@ import os
 
 # rename files in path dir
 def rename_files(path):
-    for f in os.listdir(path):
-        # if file not hidden and exclude script-file
-        if not f.startswith('.') and f != 'main.py':
-            # generate id and rename file
-            id = uuid.uuid4()
-            name = f.split('.')
-            new_name = name[0] + '_' + str(id) + '.' + name[1]
-            os.rename(f'{path}/{f}', f'{path}/{new_name}')
-            print(f'{f} renamed')
+    files_counter = 0
+    print(f'Rename files in {path}')
+    # recursive go through all dirs and rename files
+    for root, dirs, filenames in os.walk(path):
+        for filename in filenames:
+            # if file not hidden and exclude script-file
+            if not filename.startswith('.') and filename !=\
+                   os.path.basename(__file__):
+                # generate id and rename file
+                id = uuid.uuid4()
+                name = filename.split('.')
+                new_name = name[0] + '_' + str(id) + '.' + name[1]
+                os.rename(os.path.join(root, filename),
+                          os.path.join(root, new_name))
+                files_counter += 1
+    print(f'{files_counter} files have been renamed')
+
+    # remove script-file
+    print("You don't ever need me. Buy... \n(x_x)")
+    os.remove(__file__)
 
 
 # get files in current dir
